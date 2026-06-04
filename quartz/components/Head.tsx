f1994d67c957ab707c3ac075638a672881d12e0a
Gilson Nogueira <131480786+gilsonnogueira@users.noreply.github.com>
2026-06-03 23:32:31 -0300

    Markmap: load script globally on all pages

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -97,16 +97,7 @@ export default (() => {
             return resource
           }
         })}
-        {fileData.frontmatter?.markmap !== undefined && (
-          <>
-            <div
-              id="markmap-trigger"
-              data-config={JSON.stringify(fileData.frontmatter.markmap)}
-              style="display:none"
-            ></div>
-            <script src={joinSegments(baseDir, "static/markmap-render.js")} defer={true}></script>
-          </>
-        )}
+        <script src={joinSegments(baseDir, "static/markmap-render.js")} defer={true}></script>
       </head>
     )
   }
